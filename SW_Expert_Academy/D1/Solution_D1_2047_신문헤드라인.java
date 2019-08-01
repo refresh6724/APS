654f7f7791d41ea50d76c8a3f3d9c187e929fbad
refresh6724 <refresh6724@gmail.com>
2019-08-01 12:51:55 +0900

    제출일 2019-08-01 12:51

---
+++ SW_Expert_Academy/D1/Solution_D1_2047_신문헤드라인.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Solution_D1_2047_신문헤드라인 { // 제출일 2019-08-01 12:51
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next().toUpperCase();
+		System.out.println(str);
+	}
+
+}
