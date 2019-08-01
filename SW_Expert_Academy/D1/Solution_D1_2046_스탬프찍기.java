9ef8278fe255d51264baf84108ec3985fdbb9ad3
refresh6724 <refresh6724@gmail.com>
2019-08-01 12:59:43 +0900

    제출일 2019-08-01 12:58

---
+++ SW_Expert_Academy/D1/Solution_D1_2046_스탬프찍기.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Solution_D1_2046_스탬프찍기 { // 제출일 2019-08-01 12:58
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < N; i++) {
+			sb.append('#');
+		}
+		System.out.println(sb);
+	}
+
+}
