5c8b04a1f51bb596f441af20fcd1bbd684bb4f18
refresh6724 <refresh6724@gmail.com>
2019-08-08 19:45:30 +0900

    제출일 2019-08-08 19:31

---
+++ Baekjoon_Online_Judge/알고리즘/출력/Main_BOJ_11719_그대로_출력하기_2.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main { // 제출일 2019-08-08 19:31
+
+	public static void main(String[] args) throws IOException {
+		String s = null;
+		Scanner sc = new Scanner(System.in);
+//		StringBuilder sb = new StringBuilder();
+		while (sc.hasNextLine()) {
+			s = sc.nextLine();
+			System.out.println(s);
+		}
+
+	}
+
+}
