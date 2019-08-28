75cf77a5bbfc7580806ef104c4e34921f9a56f7f
refresh6724 <refresh6724@gmail.com>
2019-08-28 20:11:16 +0900

    제출일 2019-08-28 14:43

---
+++ Baekjoon_Online_Judge/알고리즘/사칙연산/Main_BOJ_11021_A+B_7.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main { // 제출일 2019-08-28 14:43
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static int answer;
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		sc.init();
+//		// 테스트케이스
+		int TC = sc.nextInt();
+		for (int tc = 1; tc <= TC; tc++) {
+			int a = sc.nextInt();
+			int b= sc.nextInt();
+			answer = a+b;
+			
+			bw.write(String.format("Case #%d: %d\n", tc, answer));
+			bw.flush();
+		}	
+	}
+
+	static class sc {
+		private static BufferedReader br;
+		private static StringTokenizer st;
+
+		static void init() throws Exception {
+			//System.setIn(new FileInputStream("input.txt"));// 테스트케이스 파일을 프로젝트 폴더에 삽입
+			br = new BufferedReader(new InputStreamReader(System.in));
+			st = new StringTokenizer(" ");
+		}
+
+		static String readLine() {
+			try {
+				return br.readLine();
+			} catch (IOException e) {
+			}
+			return null;
+		}
+
+		static String next() {
+			while (!st.hasMoreTokens()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+				}
+			}
+			return st.nextToken();
+
+		}
+
+		static int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		static long nextLong() {
+			return Long.parseLong(next());
+		}
+	}
+}
