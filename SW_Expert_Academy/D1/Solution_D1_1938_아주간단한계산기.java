259beed26d5f0da5ceb33d3b1f9b60d9ef2ea404
refresh6724 <refresh6724@gmail.com>
2019-11-07 16:59:26 +0900

    제출일 2019-11-07 16:58

---
+++ SW_Expert_Academy/D1/Solution_D1_1938_아주간단한계산기.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Solution_D1_1938_아주간단한계산기 { // 제출일 2019-11-07 16:58
+	public static void main(String[] args) {
+		// N의 약수를 오름차순으로
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println(a + b);
+		System.out.println(a - b);
+		System.out.println(a * b);
+		System.out.println(a / b);
+
+	}
+}
