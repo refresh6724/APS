5f745bfe5a6b2423a9f32bdd688b5f7070d6616f
refresh6724 <refresh6724@gmail.com>
2019-11-07 17:42:57 +0900

    제출일 2019-11-07 17:42

---
+++ SW_Expert_Academy/D1/Solution_D1_2043_서랍의비밀번호.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Solution_D1_2043_서랍의비밀번호 { // 제출일 2019-11-07 17:42 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println(a - b + 1);
+	}
+}
