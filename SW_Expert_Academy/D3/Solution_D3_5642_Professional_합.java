12f2e963e0af6ac27e343ac801e93744a8b5c937
refresh6724 <refresh6724@gmail.com>
2020-01-27 22:55:13 +0900

    제출일 2020-01-27 22:44

---
+++ SW_Expert_Academy/D3/Solution_D3_5642_Professional_합.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution_D3_5642_Professional_합 { // 제출일 2020-01-27 22:53
+
+	public static void main(String args[]) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+
+		int T;
+		T = Integer.parseInt(br.readLine().trim());
+
+		for (int test_case = 1; test_case <= T; test_case++) {
+			//
+			int n = Integer.parseInt(br.readLine().trim());
+			st = new StringTokenizer(br.readLine(), " ");
+			int answer = 0x80000000 +1;
+			int prevmax = 0;
+			int now = 0;
+			for (int i = 0; i < n; i++) {
+				now = Integer.parseInt(st.nextToken());
+				prevmax = (now > (prevmax + now)) ? now : prevmax + now;
+				if (answer < prevmax)
+					answer = prevmax;
+			}
+
+			System.out.println("#"+test_case+" "+answer);
+		}
+	}
+
+}
