f05e68f7572553d38f91063f074651d6efe1fc32
refresh6724 <refresh6724@gmail.com>
2020-03-05 21:37:35 +0900

    제출일 2020-03-05 21:35

--- SW_Expert_Academy/D4/Solution_D4_8275_햄스터.java
+++ SW_Expert_Academy/D4/Solution_D4_8275_햄스터.java
@@ -4,9 +4,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.StringTokenizer;
 
-public class Solution_D4_8275_햄스터 { // 제출일 2020-03-04 16:18
+public class Solution_D4_8275_햄스터 { // 제출일 2020-03-05 21:35
 
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -27,10 +28,10 @@ public class Solution_D4_8275_햄스터 { // 제출일 2020-03-04 16:18
 			N = Integer.parseInt(st.nextToken());
 			X = Integer.parseInt(st.nextToken());
 			M = Integer.parseInt(st.nextToken());
-			
+
 			n = new int[N];
 			cond = new int[M][3];
-			
+
 			for (int i = 0; i < M; i++) {
 				st = new StringTokenizer(br.readLine());
 				cond[i][0] = Integer.parseInt(st.nextToken());
@@ -38,14 +39,21 @@ public class Solution_D4_8275_햄스터 { // 제출일 2020-03-04 16:18
 				cond[i][2] = Integer.parseInt(st.nextToken());
 			}
 
+			Arrays.sort(cond, new Comparator<int[]>() {
+				@Override
+				public int compare(int[] o1, int[] o2) {
+					return o1[1] - o2[1];
+				}
+			});
+
 			max = -1;
 			dfs(0, 0);
-			
+
 			sb.append("#").append(t);
 			if (max > -1) {
 				sb.append(sb2);
 			} else {
-				sb.append(" ").append("-1");
+				sb.append(" ").append(max);
 			}
 			sb.append("\n");
 		}
@@ -54,46 +62,43 @@ public class Solution_D4_8275_햄스터 { // 제출일 2020-03-04 16:18
 	}
 
 	private static void dfs(int idx, int sum) {
-		
-		if(idx == N) {
-			// M 개의 조건 체크
-			if(check()) {
-				if(max < sum) {
-					max = sum;
-					sb2 = new StringBuilder();
-					for (int i = 0; i < N; i++) {
-						sb2.append(" ").append(n[i]);
-					}
+		if (idx == N) {
+			if (max > sum) {
+				return;
+			} else if (max < sum) {
+				max = sum;
+				sb2 = new StringBuilder();
+				for (int i = 0; i < N; i++) {
+					sb2.append(" ").append(n[i]);
 				}
-			}			
+			}
 			return;
 		}
-		
 		for (int i = 0; i <= X; i++) {
 			n[idx] = i;
-			dfs(idx+1, sum+i);
+			if (check(idx)) {
+				dfs(idx + 1, sum + i);
+			}
 		}
-		
 	}
 
-	private static boolean check() {
+	private static boolean check(int last) {
 		for (int i = 0; i < M; i++) {
+			int r = cond[i][1] - 1;
+			if (last > r) {
+				continue;
+			} else if (last < r) {
+				break;
+			}
 			int tmp = 0;
-			
-			int l = cond[i][0]-1;
-			int r = cond[i][1]-1;
-			int s = cond[i][2];
-			
-			for (int j = l; j <= r; j++) {
+			for (int j = cond[i][0] - 1; j <= r; j++) {
 				tmp += n[j];
 			}
-			
-			if(tmp != s) {
+			if (tmp != cond[i][2]) {
 				return false;
 			}
 		}
-		return true;		
+		return true;
 	}
-
 
 }
