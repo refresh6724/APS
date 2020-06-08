b47fc84514a9cf296c34f7e1ef5c5c5b47f1a2a4
refresh6724 <51112637+refresh6724@users.noreply.github.com>
2020-06-08 23:53:17 +0900

    제출일 2020-06-08 23:48

--- SW_Expert_Academy/D3/Solution_D3_1221_GNS.java
+++ SW_Expert_Academy/D3/Solution_D3_1221_GNS.java
@@ -8,10 +8,13 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.StringTokenizer;
 
-public class Solution_D3_1221_GNS { // 제출일 2020-06-08 23:21 36,264 kb 170 ms
+//     최초 제출일 2020-06-08 23:21 36,264 kb 170 ms
+// 1차 개선 제출일 2020-06-08 23:48 25,892 kb 149 ms
+public class Solution_D3_1221_GNS { 
 
 	static HashMap<String, Integer> map = new HashMap<>();
 	static String[] rev = new String[10];
+	static int[] num = new int[10];
 
 	public static void main(String[] args) throws Exception {
 
@@ -31,27 +34,73 @@ public class Solution_D3_1221_GNS { // 제출일 2020-06-08 23:21 36,264 kb 170 
 		}
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = null;
-		StringBuilder sb = new StringBuilder();
-//		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = null;
 		int TC = Integer.parseInt(br.readLine());
 		for (int tc = 1; tc <= TC; tc++) {
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 			st = new StringTokenizer(br.readLine());
-			sb.append(st.nextToken()).append('\n'); // #tc
+			bw.write(st.nextToken());
+			bw.write('\n');
+			sb = new StringBuilder();
+//			sb.append(st.nextToken()).append('\n'); // #tc
 			int n = Integer.parseInt(st.nextToken());
-			int[] arr = new int[n];
-			st = new StringTokenizer(br.readLine(), " ");
-			for (int i = 0; i < n; i++) {
-				arr[i] = map.get(st.nextToken());
+			// 라인 길이는 n << 2 , 끝 글자 공백
+			String a = br.readLine();
+			for (int i = 1; i < n<<2; i+=4) {
+				switch(a.charAt(i)) {
+				case 'R': // ZRO
+					num[0]++;
+					break;
+				case 'N':
+                    num[1]++;
+                    break;
+                case 'W':
+                    num[2]++;
+                    break;
+                case 'H':
+                    num[3]++;
+                    break;
+                case 'O':
+                    num[4]++;
+                    break;
+                case 'I':
+                    switch (a.charAt(i-1)) { // FIV
+                    case 'F':
+                        num[5]++;
+                        break;
+                    case 'S': // SIX
+                        num[6]++;
+                        break;
+                    case 'N': // NIN
+                        num[9]++;
+                        break;
+                    }
+                    break;
+                case 'V':
+                    num[7]++;
+                    break;
+                case 'G':
+                    num[8]++;
+                    break;
+                }
 			}
-			Arrays.sort(arr);
-			for (int i = 0; i < n - 1; i++) {
-				sb.append(rev[arr[i]]).append(' ');
+			
+			for (int i = 0; i < 10; i++) {
+				while(num[i]-- != 0) { // num을 초기화하지 않아 -값이 되어버림
+					sb.append(rev[i]).append(' ');
+				}
 			}
-			sb.append(rev[arr[n - 1]]).append('\n');
+			Arrays.fill(num, 0);
+			// ABC ABC ABC
+			// 012345678901
+			// 7 = 2 * 4 -1
+			// sb.append(' '); 
+			// #1이 계산되지 않았다
+			sb.replace((n<<2)-1, (n<<2), "\n");
+			bw.write(sb.toString());
+			bw.flush();
 		}
-		bw.write(sb.toString());
-		bw.flush();
 	}
 }
