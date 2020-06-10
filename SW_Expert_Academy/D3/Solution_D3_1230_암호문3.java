40ecf240c30ae83554c6c1f9c76b8291fe0e4241
refresh6724 <refresh6724@gmail.com>
2020-06-10 23:48:24 +0900

    제출일 2020-06-10 23:38

--- SW_Expert_Academy/D3/Solution_D3_1230_암호문3.java
+++ SW_Expert_Academy/D3/Solution_D3_1230_암호문3.java
@@ -6,27 +6,15 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.StringTokenizer;
 
-// 기존 제출일 2019-08-08 18:44 19,412 kb 98 ms
-// 개선 제출일 2020-06-10 23:43 18,660 kb 100 ms
-
-public class Solution_D3_1228_암호문1 {
-	/*
-	 * 0이상 1000000 백만 미만 수를 나열하여 만든 암호문 기능 1 삽입 명령어I x y s 앞에서부터 x의 위치 바로 다음에 y개의
-	 * 숫자를 삽입한다.
-	 * 
-	 * [입력] 첫 번째 줄 : 원본 암호문의 길이 N ( 10 ≤ N ≤ 20 의 정수) 두 번째 줄 : 원본 암호문 세 번째 줄 : 명령어의
-	 * 개수 ( 5 ≤ N ≤ 10 의 정수) 네 번째 줄 : 명령어
-	 * 
-	 * [출력] 앞 10개만
-	 */
-
+public class Solution_D3_1230_암호문3 { // 제출일 2020-06-10 23:38 32,052 kb 171 ms
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = null;
 		StringBuilder sb = new StringBuilder();
 
-		for (int t = 1; t <= 10; t++) {
+		for (int t = 1; t <= 10; t++) {			
 			sb.append("#").append(t);
 
 			int L1 = Integer.parseInt(br.readLine());
@@ -42,20 +30,43 @@ public class Solution_D3_1228_암호문1 {
 			int pos = 0;
 			st = new StringTokenizer(br.readLine());
 			for (int i = 0; i < L3; i++) {
-				st.nextToken();
-				// 포지션
-				pos = Integer.parseInt(st.nextToken());
-				// 개수 N
-				len = Integer.parseInt(st.nextToken());
-				for (int j = 0; j < len; j++) {
-					lst.add(pos + j, Integer.parseInt(st.nextToken()));
+				switch (st.nextToken()) {
+				case "I":
+					// 포지션
+					pos = Integer.parseInt(st.nextToken());
+					// 개수 N
+					len = Integer.parseInt(st.nextToken());
+					for (int j = 0; j < len; j++) {
+						lst.add(pos + j, Integer.parseInt(st.nextToken()));
+					}
+					break;
+				case "D":
+					// 포지션
+					pos = Integer.parseInt(st.nextToken());
+					// 개수 N
+					len = Integer.parseInt(st.nextToken());
+					for (int j = 0; j < len; j++) {
+						if (pos >= lst.size()) {
+							break;
+						} else {
+							lst.remove(pos);
+						}
+					}
+					break;
+				case "A":
+					// 개수 N
+					len = Integer.parseInt(st.nextToken());
+					for (int j = 0; j < len; j++) {
+						lst.add(Integer.parseInt(st.nextToken()));
+					}
+					break;
 				}
 			}
 
 			for (int k = 0; k < 10; k++) {
 				sb.append(" ").append(lst.get(k));
 			}
-			sb.append("\n");
+			sb.append("\n");			
 		}
 		bw.write(sb.toString());
 		bw.flush();
