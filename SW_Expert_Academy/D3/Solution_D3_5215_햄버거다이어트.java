9bfc521d2f42f7881081e7c886a3308bb2277740
refresh6724 <refresh6724@gmail.com>
2020-07-16 22:38:41 +0900

    제출일 2020-07-16 22:36

--- SW_Expert_Academy/D3/Solution_D3_5215_햄버거다이어트.java
+++ SW_Expert_Academy/D3/Solution_D3_5215_햄버거다이어트.java
@@ -1,44 +1,116 @@
-import java.util.Scanner;
- 
-public class Solution_D3_5215_햄버거다이어트 { // 제출일 : 2019-07-17 10:55
-    //전체 점수
-    static void selectSource(int num, int[] scoreArr, int[] calArr, int score, int cal, int calLimit) {
-        if( num != -1) {
-            cal = cal + calArr[num];
-            score = score + scoreArr[num];
-            if (score > maxScore) {
-                maxScore = score;
-            }
-        }
-        for(int i=1, size=scoreArr.length; num+i<size; i++) {
-            if(cal + calArr[num+i] > calLimit) {
-                 
-            } else {
-                selectSource(num+i, scoreArr, calArr, score, cal, calLimit);
-            }   
-        }               
-    }
-    static int maxScore = 0;
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        Scanner scan = new Scanner(System.in);
-        int totalTestCase = Integer.parseInt(scan.next());      
-        for(int test = 1; test<=totalTestCase; test++) {
-            maxScore = 0;
-            int numSource = scan.nextInt();
-            int calLimit = scan.nextInt();
-            int[] scoreArr = new int[numSource];
-            int[] calArr = new int[numSource];      
-            for(int i=0; i<numSource; i++) {
-                scoreArr[i] = scan.nextInt();
-                calArr[i] = scan.nextInt();
-            }
-             
-            int cal = 0;
-            int score = 0;
-            selectSource(-1, scoreArr, calArr, score, cal, calLimit);       
-             
-            System.out.printf("#%d %d\n",test,maxScore);
-        }       
-    }
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+// 기존 제출일 2019-07-17 10:55 25,892 kb 210 ms
+// 개선 제출일 2020-07-16 22:36 21,324 kb 167 ms
+public class Solution_D3_5215_햄버거다이어트 {
+
+	static int N, L, ans;
+	static int[] score, calory;
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = null;
+
+		Reader s = new Reader();
+
+//		int TC = Integer.parseInt(br.readLine());
+		int TC = s.nextInt();
+//		testcase: 
+		for (int tc = 1; tc <= TC; tc++) {
+
+			sb.append('#').append(tc).append(' ');
+
+			// 특정 칼로리 이하 조합에서 최대 점수 찾기
+			N = s.nextInt(); // 재료 개수
+			L = s.nextInt(); // 칼로리 제한
+			score = new int[N];
+			calory = new int[N];
+
+			for (int i = 0; i < N; i++) {
+				score[i] = s.nextInt();
+				calory[i] = s.nextInt();
+			}
+			ans = 0;
+			dfs(0, 0, 0);
+			sb.append(ans).append('\n');
+
+		}
+		bw.write(sb.toString());
+		bw.flush();
+	}
+
+	private static void dfs(int idx, int cal, int sco) {
+
+		if (cal > L) {
+			return;
+		}
+
+		if (idx == N) {
+			ans = Math.max(ans, sco);
+			return;
+		}
+
+		dfs(idx + 1, cal, sco); // 선택하지 않은 경우
+		dfs(idx + 1, cal + calory[idx], sco + score[idx]);
+	}
+
+	/**
+	 * https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+	 */
+	static class Reader {
+		final private int BUFFER_SIZE = 1 << 16;
+		private DataInputStream din;
+		private byte[] buffer;
+		private int bufferPointer, bytesRead;
+
+		public Reader() {
+			din = new DataInputStream(System.in);
+			buffer = new byte[BUFFER_SIZE];
+			bufferPointer = bytesRead = 0;
+		}
+
+		public String readLine() throws IOException {
+			byte[] buf = new byte[64]; // line length
+			int cnt = 0, c;
+			while ((c = read()) != -1) {
+				if (c == '\n')
+					break;
+				buf[cnt++] = (byte) c;
+			}
+			return new String(buf, 0, cnt);
+		}
+
+		public int nextInt() throws IOException {
+			int ret = 0;
+			byte c = read();
+			while (c <= ' ')
+				c = read();
+			do {
+				ret = ret * 10 + c - '0';
+			} while ((c = read()) >= '0' && c <= '9');
+			return ret;
+		}
+
+		private void fillBuffer() throws IOException {
+			bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+			if (bytesRead == -1)
+				buffer[0] = -1;
+		}
+
+		private byte read() throws IOException {
+			if (bufferPointer == bytesRead)
+				fillBuffer();
+			return buffer[bufferPointer++];
+		}
+	}
+
 }
