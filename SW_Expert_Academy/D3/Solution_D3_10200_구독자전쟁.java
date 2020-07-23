5515cf8e782f1e0ae62455357ced729b6b355d4f
refresh6724 <refresh6724@gmail.com>
2020-07-23 20:03:11 +0900

    제출일 2020-07-23 20:02

---
+++ SW_Expert_Academy/D3/Solution_D3_10200_구독자전쟁.java
@@ -0,0 +1,92 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Solution_D3_10200_구독자전쟁 { // 제출일 2020-07-23 20:02 16,088 kb 97 ms
+		
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+
+		Reader s = new Reader();
+
+//		int TC = Integer.parseInt(br.readLine());
+		int TC = s.nextInt();
+		for (int tc = 1; tc <= TC; tc++) {
+
+			sb.append('#').append(tc).append(' ');
+
+			int U = s.nextInt();
+			int A = s.nextInt();
+			int B = s.nextInt();
+			
+			// A와 B의 교집합 최소 크기와 최대 크기
+			int max = Math.min(A, B);
+			int min = A+B-U;
+
+			sb.append(max).append(' ').append((min>0)?min:0).append('\n');
+
+		}
+		bw.write(sb.toString());
+		bw.flush();
+	}
+
+	/**
+	 * https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+	 */
+	static class Reader {
+		final private int BUFFER_SIZE = 1 << 16;
+		private DataInputStream din;
+		private byte[] buffer;
+		private int bufferPointer, bytesRead;
+
+		public Reader() {
+			din = new DataInputStream(System.in);
+			buffer = new byte[BUFFER_SIZE];
+			bufferPointer = bytesRead = 0;
+		}
+
+		public String readLine() throws IOException {
+			byte[] buf = new byte[64]; // line length
+			int cnt = 0, c;
+			while ((c = read()) != -1) {
+				if (c == '\n')
+					break;
+				buf[cnt++] = (byte) c;
+			}
+			return new String(buf, 0, cnt);
+		}
+
+		public int nextInt() throws IOException {
+			int ret = 0;
+			byte c = read();
+			while (c <= ' ')
+				c = read();
+			do {
+				ret = ret * 10 + c - '0';
+			} while ((c = read()) >= '0' && c <= '9');
+			return ret;
+		}
+
+		private void fillBuffer() throws IOException {
+			bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+			if (bytesRead == -1)
+				buffer[0] = -1;
+		}
+
+		private byte read() throws IOException {
+			if (bufferPointer == bytesRead)
+				fillBuffer();
+			return buffer[bufferPointer++];
+		}
+	}
+
+}
