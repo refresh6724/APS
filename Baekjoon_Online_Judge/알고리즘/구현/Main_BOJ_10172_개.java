e08a136488a9d40447b7021570939c58a172bfa9
refresh6724 <refresh6724@gmail.com>
2020-09-07 22:39:17 +0900

    제출일 2020-09-07 22:34

---
+++ Baekjoon_Online_Judge/알고리즘/구현/Main_BOJ_10172_개.java
@@ -0,0 +1,11 @@
+public class Main_BOJ_10172_개 { // 제출일 2020-09-07 22:34
+
+	public static void main(String[] args) throws Exception {
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+	}
+
+}
