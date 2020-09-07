baf92e1b8f65e0c590d3baf637a47a915113cce4
refresh6724 <refresh6724@gmail.com>
2020-09-07 22:52:43 +0900

    제출일 2020-09-07 22:51

---
+++ Baekjoon_Online_Judge/알고리즘/사칙연산/Main_BOJ_10430_나머지.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_BOJ_10430_나머지 { // 제출일 2020-09-07 22:51
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		System.out.println((a+b)%c);
+		System.out.println(((a%c)+(b%c))%c);
+		System.out.println((a*b)%c);
+		System.out.println(((a%c)*(b%c))%c);
+	}
+
+}
