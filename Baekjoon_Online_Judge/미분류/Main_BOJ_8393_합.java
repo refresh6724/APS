ee82673790a4d7a694a0e43df9cd577786a9f53a
refresh6724 <refresh6724@gmail.com>
2020-09-12 23:06:27 +0900

    제출일 2020-09-12 23:06

---
+++ Baekjoon_Online_Judge/미분류/Main_BOJ_8393_합.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main_BOJ_8393_합 { // 제출일 2020-09-12 23:06
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+
+		int N = Integer.parseInt(br.readLine());
+		sb.append((N*(N+1)/2));
+		bw.write(sb.toString());
+		bw.flush();
+	}
+
+}
