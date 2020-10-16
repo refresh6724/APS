17379ec8862624ee64dd4f5d1d1f67fe0fb813c5
refresh6724 <refresh6724@gmail.com>
2020-10-16 22:55:19 +0900

    제출일 2020-10-16 22:54

---
+++ Baekjoon_Online_Judge/미분류/Main_BOJ_11654_아스키코드.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * https://www.acmicpc.net/problem/11654
+ */
+public class Main_BOJ_11654_아스키코드 { // 제출일 2020-10-16 22:54
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int a = br.read();
+		System.out.println(a);
+	}
+}
