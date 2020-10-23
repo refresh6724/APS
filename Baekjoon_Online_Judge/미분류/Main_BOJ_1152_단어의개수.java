337d97b5535c9796280bece33ed613af21a9316d
refresh6724 <refresh6724@gmail.com>
2020-10-23 23:09:39 +0900

    제출일 2020-10-23 23:09

--- Baekjoon_Online_Judge/미분류/Main_BOJ_1152_단어의개수.java
+++ Baekjoon_Online_Judge/미분류/Main_BOJ_1152_단어의개수.java
@@ -5,10 +5,15 @@ import java.io.InputStreamReader;
 /**
  * https://www.acmicpc.net/problem/1152
  */
-public class Main_BOJ_1152_단어의개수 { // 제출일 2020-10-23 23:03
+public class Main_BOJ_1152_단어의개수 { // 제출일 2020-10-23 23:09
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println(br.readLine().trim().split(" ").length);
+		String input = br.readLine();
+		if(input.length()== 1 && input.charAt(0) == ' ') { // 공백 한글자 조건 빠짐
+			System.out.println(0);
+		} else {			
+			System.out.println(input.trim().split(" ").length);
+		}
 	}
 }
