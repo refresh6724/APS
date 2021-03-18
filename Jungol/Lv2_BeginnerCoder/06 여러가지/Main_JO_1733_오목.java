4ae75a728f3e1e13f298adb4b96ec0a49d08c639
refresh6724 <refresh6724@gmail.com>
2021-03-18 22:45:22 +0900

    제출일 2021-03-18 22:42

--- Jungol/Lv2_BeginnerCoder/06 여러가지/Main_JO_1733_오목.java
+++ Jungol/Lv2_BeginnerCoder/06 여러가지/Main_JO_1733_오목.java
@@ -1,86 +1,145 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-public class Main_JO_1733_오목 { // 제출일 2019-11-08 11:06
+public class Main_JO_1733_오목 { // 제출일 2021-03-18 22:42
 
-	static int[][] map = new int[21][21]; // 1~19번 사용
-	static int[][][] one = new int[21][21][4];
-	static int[][][] two = new int[21][21][4];
-	
-	// 4방향 탐색		좌하향, 우향, 우하향, 하향
-	static int[] dr = {1, 0, 1, 1};
-	static int[] dc = {-1, 1, 1, 0};
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		for (int i = 1; i < 20; i++) {
-			for (int j = 1; j < 20; j++) {
-				map[i][j] = sc.nextInt();
-				if(map[i][j] == 0) {
-					continue;
-				} else if(map[i][j] == 1) {
-					for (int k = 0; k < 4; k++) {
-						two[i][j][k]=0;
-						one[i][j][k]++;
-						one[i+dr[k]][j+dc[k]][k]+=one[i][j][k];
-					}
-				} else if (map[i][j] == 2) {
-					for (int k = 0; k < 4; k++) {
-						one[i][j][k]=0;
-						two[i][j][k]++;
-						two[i+dr[k]][j+dc[k]][k]+=two[i][j][k];
-					}
-				}
-				
+	static int[][] board = new int[19][19];
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+
+		// 승리 판별
+		// 검정 1 흰색 2 승부가 나지 않은 경우 0
+		// 6목은 승부가 나지 않은 것
+		// 바둑판 전체에 대해서 해당 돌
+		// 좌상이 없다면 우하로 체크
+		// 위가 없다면 아래로 체크
+		// 우상이 없다면 좌하로 체크
+		// 좌가 없다면 우로 체크
+
+		// 오답 수정
+		// 가장 왼쪽에 있는 것을 출력하므로 좌하가 없다면 우상으로 체크해야함
+
+		for (int i = 0; i < 19; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < 19; j++) {
+				board[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		
-		
-//		for (int i = 1; i < 20; i++) {
-//			for (int j = 1; j < 19; j++) {
-//				System.out.print(two[i][j][3]+" ");
-//			}
-//			System.out.println();
-//		}
-//		
-		
-			
-		
-		boolean find = false;
-		loop:
-		for (int i = 1; i < 20; i++) {
-			for (int j = 1; j < 20; j++) {
-				// 5목이 있으면 6목을 확인하고 없다면 출력
-				for (int k = 0; k < 4; k++) {
-					if(one[i][j][k] == 5 && one[i+dr[k]][j+dc[k]][k] != 6) {
-						find = true;
-						if(k == 0) { 
-							System.out.printf("1\n%d %d", i, j);							
-						} else if (k == 1) {
-							System.out.printf("1\n%d %d", i, j-4);
-						} else if (k == 2) {
-							System.out.printf("1\n%d %d", i-4, j-4);
-						} else if (k == 3) {
-							System.out.printf("1\n%d %d", i-4, j);
-						}
-						break loop;
-					} else if(two[i][j][k] == 5 && two[i+dr[k]][j+dc[k]][k] != 6) {
-						find = true;
-						if(k == 0) { 
-							System.out.printf("2\n%d %d", i, j);							
-						} else if (k == 1) {
-							System.out.printf("2\n%d %d", i, j-4);
-						} else if (k == 2) {
-							System.out.printf("2\n%d %d", i-4, j-4);
-						} else if (k == 3) {
-							System.out.printf("2\n%d %d", i-4, j);
-						}
-						break loop;
-					} 
-				}
-			}
+
+		if (!go()) {
+			sb.append(0);
 		}
-		if(!find) System.out.println(0);
-		
-		
+
+		bw.write(sb.toString());
+		bw.flush();
 	}
-	
+
+	private static boolean go() {
+		for (int i = 0; i < 19; i++) {
+			for (int j = 0; j < 19; j++) {
+				if (board[i][j] != 0) {
+					for (int dir = 0; dir < 4; dir++) {
+						if (makeFive(i, j, dir)) {
+							sb.append(board[i][j]).append('\n');
+							sb.append(i + 1).append(' ').append(j + 1);
+							return true;
+						}
+					}
+				}
+			}
+		}
+		return false;
+	}
+
+	private static boolean makeFive(int i, int j, int dir) {
+		if (dir == 0) { // 좌->우
+			// 0번돌이 존재할 때 1번돌이 0번돌과 같은 색이면 안된다
+			if (j - 1 >= 0 && board[i][j - 1] == board[i][j]) {
+				return false;
+			}
+			// 6번돌이 존재할 때 1번돌이 6번돌과 같은 색이면 안된다
+			if (j + 5 < 19 && board[i][j] == board[i][j + 5]) {
+				return false;
+			}
+			// 범위
+			if (j + 5 > 19) {
+				return false;
+			}
+			for (int k = 1; k < 5; k++) {
+				if (board[i][j] != board[i][j + k]) {
+					return false;
+				}
+			}
+			return true;
+		} else if (dir == 1) { // 좌상 -> 우하
+			// 0번돌이 존재할 때 1번돌이 0번돌과 같은 색이면 안된다
+			if (i - 1 >= 0 && j - 1 >= 0 && board[i - 1][j - 1] == board[i][j]) {
+				return false;
+			}
+			// 6번돌이 존재할 때 1번돌이 6번돌과 같은 색이면 안된다
+			if (i + 5 < 19 && j + 5 < 19 && board[i][j] == board[i + 5][j + 5]) {
+				return false;
+			}
+			// 범위
+			if (i + 5 > 19 || j + 5 > 19) {
+				return false;
+			}
+			for (int k = 1; k < 5; k++) {
+				if (board[i][j] != board[i + k][j + k]) {
+					return false;
+				}
+			}
+			return true;
+		} else if (dir == 2) { // 상 -> 하
+			// 0번돌이 존재할 때 1번돌이 0번돌과 같은 색이면 안된다
+			if (i - 1 >= 0 && board[i - 1][j] == board[i][j]) {
+				return false;
+			}
+			// 6번돌이 존재할 때 1번돌이 6번돌과 같은 색이면 안된다
+			if (i + 5 < 19 && board[i][j] == board[i + 5][j]) {
+				return false;
+			}
+			// 범위
+			if (i + 5 > 19) {
+				return false;
+			}
+
+			for (int k = 1; k < 5; k++) {
+				if (board[i][j] != board[i + k][j]) {
+					return false;
+				}
+			}
+			return true;
+		} else if (dir == 3) { // 우상 -> 좌하 ==> 좌하 -> 우상
+			// 0번돌이 존재할 때 1번돌이 0번돌과 같은 색이면 안된다
+			if (i + 1 < 19 && j - 1 >= 0 && board[i + 1][j - 1] == board[i][j]) {
+				return false;
+			}
+			// 6번돌이 존재할 때 1번돌이 6번돌과 같은 색이면 안된다
+			if (i - 5 >= 0 && j + 5 < 19 && board[i][j] == board[i - 5][j + 5]) {
+				return false;
+			}
+			// 범위
+			if (i - 5 < 0 || j + 5 > 19) {
+				return false;
+			}
+			for (int k = 1; k < 5; k++) { // 우 상
+				if (board[i][j] != board[i - k][j + k]) {
+					return false;
+				}
+			}
+			return true;
+		}
+		return false;
+	}
+
 }
