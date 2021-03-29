439b3cfa67f6737a511b1fc2a976898e410f0f3c
refresh6724 <refresh6724@gmail.com>
2021-03-29 23:27:04 +0900

    제출일 2021-03-29 23:26

--- Jungol/Lv2_BeginnerCoder/08 재귀/Main_JO_1169_주사위던지기1.java
+++ Jungol/Lv2_BeginnerCoder/08 재귀/Main_JO_1169_주사위던지기1.java
@@ -1,117 +1,116 @@
-import java.util.Scanner;
- 
-/*
-  
-주사위를 던진 횟수 N과 출력형식 M을 입력 받아서 M의 값에 따라 각각 아래와 같이 출력하는 프로그램을 작성하시오.
-  
-M = 1 : 주사위를 N번 던져서 나올 수 있는 모든 경우
-M = 2 : 주사위를 N번 던져서 중복이 되는 경우를 제외하고 나올 수 있는 모든 경우
-M = 3 : 주사위를 N번 던져서 모두 다른 수가 나올 수 있는 모든 경우
-  
-* 중복의 예
-1 1 2 와 중복 : 1 2 1, 2 1 1
-1 2 3 과 중복 : 1 3 2, 2 1 3, 2 3 1, 3 1 2
- 
- */
-public class Main {
-     
-    static int N;
-    static int M;
-    static int[] dices; 
-    static int bit = 0;
-    static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
-        dices = new int[N];
-     
-        switch(M) {
-        case 1:
-            rollAll(0);
-            break;
-        case 2:
-            rollDiff(0, 1);
-            break;
-        case 3: 
-            rollPermutation(0);
-            break;
-        }
-        System.out.println(sb);
-    }
-    static void rollAll(int index) {
-        // 주사위를 N번 던져 나올 수 있는 모든 경우
-        if(index == N) {
-            for (int i = 0; i < N-1; i++) {
-                sb.append(dices[i]).append(" ");
-            }
-            sb.append(dices[N-1]).append("\n");
-            return;
-        }       
-        for (int i = 1; i <= 6; i++) {
-            dices[index] = i;
-            rollAll(index+1);
-        }   
-    }
-     
-    static void rollCombination(int index, int dice) {
-        // 중복 없이, 1 2 3 과 중복 : 1 3 2, 2 1 3, 2 3 1, 3 1 2
-        // 종료조건
-        if(index == N) {
-            for (int i = 0; i < N-1; i++) {
-                sb.append(dices[i]).append(" ");
-            }
-            sb.append(dices[N-1]).append("\n");
-            return;
-        }
-        if(dice == 7) {
-            return;
-        }
-        dices[index] = dice;
-        rollCombination(index+1, dice+1);
-        rollCombination(index, dice+1); // 이대로 넘겨서 덮어쓰기
-         
-    }
-     
-    static void rollDiff(int index, int dice) {
-        // 서로 다른 조합(원소 중복 허용)
-        // 종료조건
-        if(index == N) {
-            for (int i = 0; i < N-1; i++) {
-                sb.append(dices[i]).append(" ");
-            }
-            sb.append(dices[N-1]).append("\n");
-            return;
-        }
-        if(dice == 7) {
-            return;
-        }
-        dices[index] = dice;
-        rollDiff(index+1, dice);        
-        rollDiff(index, dice+1);
-         
-    }
-     
-    static void rollPermutation(int index) {
-        // 서로 다른 조합(원소 중복 허용)
-        // 종료조건
-        if(index == N) {
-            for (int i = 0; i < N-1; i++) {
-                sb.append(dices[i]).append(" ");
-            }
-            sb.append(dices[N-1]).append("\n");
-            return;
-        }
-        for(int i=1; i<=6; i++) {
-            if((bit & (1<<i)) != (1<<i)) { // 미방문시에만
-                dices[index] = i;
-                bit |= (1<<i);
-                rollPermutation(index+1);
-                bit ^= (1<<i);
-            }
-        }
-    }
-     
-     
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main_JO_1169_주사위던지기1 { // 제출일 2021-03-29 23:21
+
+	static class Dice {
+		int[] pips;
+
+		public Dice() {
+			// TODO Auto-generated constructor stub
+		}
+
+		public Dice(int size) {
+			this.pips = new int[size];
+		}
+
+		@Override
+		public String toString() {
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < pips.length - 1; i++) {
+				sb.append(pips[i]).append(' ');
+			}
+			sb.append(pips[pips.length - 1]).append('\n');
+			return sb.toString();
+		}
+	}
+
+	static int N;
+	static Dice dice;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		dice = new Dice(N);
+
+		switch (M) {
+		case 1:
+			rollAll(0);
+			break;
+		case 2:
+			rollCombination(0, 1);
+			break;
+		case 3:
+			rollPermutation(0, 0);
+			break;
+		}
+
+		bw.write(sb.toString());
+		bw.flush();
+	}
+
+	static void rollAll(int index) {
+
+		// 주사위를 N번 던져 나올 수 있는 모든 경우
+		// 111 부터 666까지
+
+		if (index == N) {
+			sb.append(dice.toString());
+			return;
+		}
+		for (int i = 1; i <= 6; i++) {
+			dice.pips[index] = i;
+			rollAll(index + 1);
+		}
+	}
+
+	static void rollCombination(int index, int pip) {
+
+		// 1번에서 중복을 제외하고
+		// 111 에서 666까지 즉 566 다음 611이 아니라 666 인 경우
+		// 1 다음에는 1에서 6까지 나올 수 있지만 6 다음에는 6밖에 못온다
+
+		if (index == N) {
+			sb.append(dice.toString());
+			return;
+		}
+
+		if (pip == 7) {
+			return;
+		}
+
+		dice.pips[index] = pip;
+		rollCombination(index + 1, pip);
+		rollCombination(index, pip + 1); // 이대로 넘겨서 덮어쓰기
+
+	}
+
+	static void rollPermutation(int index, int bit) {
+
+		// 서로 다른 조합(원소 중복 허용)
+		// 123 부터 321 ... 654 까지
+
+		if (index == N) {
+			sb.append(dice.toString());
+			return;
+		}
+
+		for (int i = 1; i <= 6; i++) {
+			if ((bit & (1 << i)) != (1 << i)) { // 미방문시에만
+				dice.pips[index] = i;
+				bit |= (1 << i);
+				rollPermutation(index + 1, bit);
+				bit ^= (1 << i);
+			}
+		}
+	}
+
 }
--- Jungol/Lv2_BeginnerCoder/08 재귀/Main_JO_1175_주사위던지기2.java
+++ Jungol/Lv2_BeginnerCoder/08 재귀/Main_JO_1175_주사위던지기2.java
@@ -1,45 +1,64 @@
-import java.util.Scanner;
- 
-/*
- * 자연수 N과 M을 입력 받아서 주사위를 N번 던져서 나온 눈의 합이 M이 나올 수 있는 모든 경우를 출력하는 프로그램을 작성하시오
- * 첫 줄에 주사위를 던진 횟수 N(2<=7) 과 눈의 합 M(1<=40)이 들어온다.
- * 주사위를 던진 횟수의 합이 M이 되는 경우를 모두 출력한다
- * 입력 예) 3 10  3번던져 10이 나오는 경우
- * 출력 예) 1 3 6
- *          1 4 5 ...
- * 
- * 
- */
-public class Main {
-     
-    static int N;
-    static int M;
-    static int[] dices;
-    static int sum;
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
-        dices = new int[N];
-        sum = 0;
-        roll(0); // sum을 매개변수로 주면 stackoverflow 발생
-    }
-    static void roll(int index) {
-        if(index == N) {
-            if(sum == M) { 
-                for (int i = 0; i < N-1; i++) {
-                    System.out.print(dices[i]+" ");
-                }
-                System.out.print(dices[N-1]+"\n");
-            }
-            return;
-        }               
-        for(int i=1; i<=6; i++) {
-            sum += i;
-            dices[index] = i;
-            roll(index+1);  
-            sum -= i;
-        }       
-    }
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main_JO_1175_주사위던지기2 { // 제출일 2021-03-29 23:26
+	static class Dice {
+		int[] pips;
+
+		public Dice() {
+			// TODO Auto-generated constructor stub
+		}
+
+		public Dice(int size) {
+			this.pips = new int[size];
+		}
+
+		@Override
+		public String toString() {
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < pips.length - 1; i++) {
+				sb.append(pips[i]).append(' ');
+			}
+			sb.append(pips[pips.length - 1]).append('\n');
+			return sb.toString();
+		}
+	}
+
+	static int N;
+	static int M;
+	static Dice dice;
+	static StringBuilder sb;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		// 주사위를 던짓 횟수 N(2이상 7이하)과 눈의 합 M(1이상 40이하)
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		dice = new Dice(N);
+		sb = new StringBuilder();
+
+		roll(0, 0);
+
+		bw.write(sb.toString());
+		bw.flush();
+	}
+
+	static void roll(int index, int sum) {
+		if (index == N) {
+			if (sum == M) {
+				sb.append(dice.toString());
+			}
+			return;
+		}
+		for (int i = 1; i <= 6; i++) {
+			dice.pips[index] = i;
+			roll(index + 1, sum + i);
+		}
+	}
 }
