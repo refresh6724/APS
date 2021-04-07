d7b9fcea3d7e4a5d2a8fbcb3ae005ade90232ed7
refresh6724 <refresh6724@gmail.com>
2021-04-08 01:28:11 +0900

    제출일 2021-04-08 01:21

--- Jungol/Lv3_IntermediateCoder/04 그래프탐색-BFS/Main_JO_1082_화염에서탈출SLIKAR.java
+++ Jungol/Lv3_IntermediateCoder/04 그래프탐색-BFS/Main_JO_1082_화염에서탈출SLIKAR.java
@@ -6,7 +6,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main_JO_1082_화염에서탈출SLIKAR { // 제출일 2021-04-07 23:57
+public class Main_JO_1082_화염에서탈출SLIKAR { // 제출일 2021-04-08 01:21
 
 	// 맵은 R행 C열
 	// 시작 'S' 도착 'D'
@@ -14,35 +14,29 @@ public class Main_JO_1082_화염에서탈출SLIKAR { // 제출일 2021-04-07 23:
 	static class Node {
 		int r;
 		int c;
-		char type;
 		int step;
 
 		public Node() {
 			// TODO Auto-generated constructor stub
 		}
 
-		public Node(int r, int c, char type, int step) {
+		public Node(int r, int c, int step) {
 			super();
 			this.r = r;
 			this.c = c;
-			this.type = type;
 			this.step = step;
 		}
 
 		@Override
 		public String toString() {
 			StringBuilder builder = new StringBuilder();
-			builder.append("Node [r=").append(r).append(", c=").append(c).append(", type=").append(type).append(", step=").append(step).append("]");
+			builder.append("Node [r=").append(r).append(", c=").append(c).append(", step=").append(step).append("]");
 			return builder.toString();
 		}
-		
-		
-
 	}
 
 	static int r, c, answer;
 	static char[][] map;
-	static boolean[][] visited;
 	static Queue<Node> move, fire;
 	static int[] dr = { -1, 1, 0, 0 };
 	static int[] dc = { 0, 0, -1, 1 };
@@ -63,62 +57,73 @@ public class Main_JO_1082_화염에서탈출SLIKAR { // 제출일 2021-04-07 23:
 		answer = 0;
 
 		map = new char[r][];
-		for (int i = 0; i < r; i++) {
-			map[i] = br.readLine().toCharArray();
-		}
-		visited = new boolean[r][c];
-
 		move = new LinkedList<>();
 		fire = new LinkedList<>();
 		for (int i = 0; i < r; i++) {
+			map[i] = br.readLine().toCharArray();
 			for (int j = 0; j < c; j++) {
-				if (map[i][j] == 'S') {
-					move.add(new Node(i, j, 'S', 0));
-				} else if (map[i][j] == '*') {
-					fire.add(new Node(i, j, '*', 0));
+				if (map[i][j] == '*') { // 빈도수가 높은 것을 앞으로
+					fire.add(new Node(i, j, 0));
+				} else if (map[i][j] == 'S') { // 딱 한 번 나온다
+					move.add(new Node(i, j, 0));
 				}
 			}
 		}
-		fire.add(move.poll());
-//		int f = fire.size();
-//		for (int i = 0; i < f; i++) {
-//			move.add(fire.poll());
-//		}
 	}
 
 	private static void run() {
-		int nr = 0;
-		int nc = 0;
-		while (!move.isEmpty()) {
-			Node node = move.poll();				
-			if(node.type == '*') {
-				if(map[node.r][node.c] == 'D') {
-					continue;
-				}
-				map[node.r][node.c] = '*';					
-			} else if(node.type == 'S') {
-				if(map[node.r][node.c] == '*') {
-					continue;
-				}
-				if (map[node.r][node.c] == 'D') {
-					answer = node.step;
-					return;
-				}
-				map[node.r][node.c] = 'S';
-				visited[node.r][node.c] = true;
-			}
-						
-			for (int i = 0; i < 4; i++) {
-				nr = node.r + dr[i];
-				nc = node.c + dc[i];
-				if(nr < 0 || nc < 0 || nr == r || nc == c || map[nr][nc] == 'X' || map[nr][nc] == '*' || visited[nr][nc]) {
-					continue;
-				}				
-				move.add(new Node(nr, nc, node.type, node.step+1));			
-			}
+		while (!move.isEmpty() && movePhase()) {
+			firePhase();
 		}
 	}
 
+	private static boolean movePhase() {
+		int nr = 0;
+		int nc = 0;
+		int m = move.size();
+		for (int i = 0; i < m; i++) {
+			Node node = move.poll();
+			if (map[node.r][node.c] != 'S') {
+				continue;
+			}
+			for (int j = 0; j < 4; j++) {
+				nr = node.r + dr[j];
+				nc = node.c + dc[j];
+				if (nr < 0 || nc < 0 || nr == r || nc == c) {
+					continue;
+				}
+				if (map[nr][nc] == '.') {
+					map[nr][nc] = 'S';
+					move.add(new Node(nr, nc, node.step + 1));
+				} else if (map[nr][nc] == 'D') {
+					answer = node.step + 1;
+					move = new LinkedList<>();
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	private static void firePhase() {
+		int nr = 0;
+		int nc = 0;
+		int f = fire.size();
+		for (int i = 0; i < f; i++) {
+			Node node = fire.poll();
+			for (int j = 0; j < 4; j++) {
+				nr = node.r + dr[j];
+				nc = node.c + dc[j];
+				if (nr < 0 || nc < 0 || nr == r || nc == c) {
+					continue;
+				}
+				if (map[nr][nc] == '.' || map[nr][nc] == 'S') {
+					map[nr][nc] = '*';
+					fire.add(new Node(nr, nc, 0));
+				}
+			}
+		}
+	}
 
 	private static void output() throws Exception {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
