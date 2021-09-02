b575bc1bcf9a0de5fd75d6699f4618e42f428e35
refresh6724 <refresh6724@gmail.com>
2021-09-02 23:59:11 +0900

    제출일 2021-09-02 23:58

---
+++ Jungol/Lv1_LCoder_Python/py10_출력/Main_JO_701_출력_자가진단2-1.py
@@ -0,0 +1,5 @@
+# print 문장 세 개를 사용하여 다음과 같이 출력되는 프로그램을 작성하라.​
+
+print('Score:')
+print('100')
+print('GREAT!')
---
+++ Jungol/Lv1_LCoder_Python/py10_출력/Main_JO_702_출력_자가진단2-2.py
@@ -0,0 +1,6 @@
+# 다음과 같이 출력되는 프로그램을 작성하라.​
+
+print('My height')
+print('170')
+print('My weight')
+print('68.6')
