9b287afd63214e5fe856de72cfa351d1b758a64f
refresh6724 <refresh6724@gmail.com>
2021-12-10 23:04:14 +0900

    제출일 2021-12-10 23:02

---
+++ HackerRank/Practice/InterviewPreparationKit/StringManipulation/Solution_AlternatingCharacters_Easy.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.IntStream;
+
+class Result {
+
+	public static int alternatingCharacters(String s) {
+		int delete = 0;
+		char prev = 'C';
+		for (char x : s.toCharArray()) {
+			if (prev == x) {
+				delete++;
+			} else {
+				prev = x;
+			}
+		}
+		return delete;
+	}
+
+}
+
+public class Solution_AlternatingCharacters_Easy {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+		int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+		IntStream.range(0, q).forEach(qItr -> {
+			try {
+				String s = bufferedReader.readLine();
+
+				int result = Result.alternatingCharacters(s);
+
+				bufferedWriter.write(String.valueOf(result));
+				bufferedWriter.newLine();
+			} catch (IOException ex) {
+				throw new RuntimeException(ex);
+			}
+		});
+
+		bufferedReader.close();
+		bufferedWriter.close();
+	}
+}
